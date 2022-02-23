67ec025043ac122c1379c2f16a51ba9963cbf613
Yander <85867084+CLOUDYander@users.noreply.github.com>
2022-02-23 16:03:39 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -27,3 +27,21 @@ function clean_input2_2(){
     document.getElementById("name_input2_2").value = "";
     document.getElementById("name_input2_2").type = "password";
 }
+
+
+
+$(document).ready(function() {
+    $("body").css("display", "none");
+
+    $("body").fadeIn(2000);
+
+	$("a").click(function(event){
+		event.preventDefault();
+		linkLocation = this.href;
+		$("body").fadeOut(1000, redirectPage);
+	});
+
+	function redirectPage() {
+		window.location = linkLocation;
+	}
+});
