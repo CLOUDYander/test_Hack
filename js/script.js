662d1f7844081b4a7643787650a1fcad7097bc2c
Yander <85867084+CLOUDYander@users.noreply.github.com>
2022-02-25 21:52:03 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,47 +1,70 @@
-function Ohran0( ) { location.href = 'index5.html' };
-function Ohran1( ) { location.href = 'index2.html' };
-function Ohran2( ) { location.href = 'index3.html' };
-function Ohran3( ) { location.href = 'index4.html' };
-function Ohran4( ) { location.href = 'index6.html' };
-function Ohran5( ) { location.href = 'index7.html' };
-function Ohran6( ) { location.href = 'index8.html' };
-function Ohran7( ) { location.href = 'index13.html' };
-function Ohran8( ) { location.href = 'index8.html' };
-function Ohran9( ) { location.href = 'index14.html' };
-function Ohran10( ) { location.href = 'index11.html' };
-function Ohran11( ) { location.href = 'index12.html' };
-function Ohran12( ) { location.href = 'index11.html' };
-
-
-function clean_input(){
-    document.getElementById("name_input").value = "";
+function Ohran0() {
+  location.href = "index5.html";
 }
-function clean_input2(){
-    document.getElementById("name_input2").value = "";
+function Ohran1() {
+  location.href = "index2.html";
 }
-function clean_input3(){
-    document.getElementById("name_input3").value= "";
+function Ohran2() {
+  location.href = "index3.html";
+}
+function Ohran3() {
+  location.href = "index4.html";
+}
+function Ohran4() {
+  location.href = "index6.html";
+}
+function Ohran5() {
+  location.href = "index7.html";
+}
+function Ohran6() {
+  location.href = "index8.html";
+}
+function Ohran7() {
+  location.href = "index13.html";
+}
+function Ohran8() {
+  location.href = "index8.html";
+}
+function Ohran9() {
+  location.href = "index14.html";
+}
+function Ohran10() {
+  location.href = "index11.html";
+}
+function Ohran11() {
+  location.href = "index12.html";
+}
+function Ohran12() {
+  location.href = "index11.html";
 }
 
-function clean_input2_2(){
-    document.getElementById("name_input2_2").value = "";
-    document.getElementById("name_input2_2").type = "password";
+function clean_input() {
+  document.getElementById("name_input").value = "";
+}
+function clean_input2() {
+  document.getElementById("name_input2").value = "";
+}
+function clean_input3() {
+  document.getElementById("name_input3").value = "";
 }
 
+function clean_input2_2() {
+  document.getElementById("name_input2_2").value = "";
+  document.getElementById("name_input2_2").type = "password";
+}
 
+$(document).ready(function () {
+  $("body").css("display", "none");
 
-$(document).ready(function() {
-    $("body").css("display", "none");
+  $("body").fadeIn(1500);
 
-    $("body").fadeIn(2000);
+  $("a").click(function (event) {
+    event.preventDefault();
+    linkLocation = this.href;
+    $("body").fadeOut(500, redirectPage);
+  });
 
-	$("a").click(function(event){
-		event.preventDefault();
-		linkLocation = this.href;
-		$("body").fadeOut(1000, redirectPage);
-	});
-
-	function redirectPage() {
-		window.location = linkLocation;
-	}
+  function redirectPage() {
+    window.location = linkLocation;
+  }
 });
